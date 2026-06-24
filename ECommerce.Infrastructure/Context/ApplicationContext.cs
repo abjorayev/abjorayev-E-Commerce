d58c200f7ff15268d8eaa1ed944f79646cd5f1b2
Abdusamad <juraevabdusamad517@gmail.com>
2026-06-24 10:41:29 +0500

    db connected

---
+++ ECommerce.Infrastructure/Context/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Infrastructure.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) 
+        { 
+             
+        }
+    }
+}
