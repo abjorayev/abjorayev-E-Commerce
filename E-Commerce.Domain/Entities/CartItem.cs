fe89cd390ab482958d45fe07d346de8614c6c51e
Abdusamad <juraevabdusamad517@gmail.com>
2026-06-24 15:40:26 +0500

    first migration and all entities added

---
+++ E-Commerce.Domain/Entities/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Entities
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int ProductCount { get; set; }
+        public int CartId { get; set; }
+        public Cart Cart { get; set; }
+    }
+}
---
+++ E-Commerce.Domain/Entities/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public int TotalAmount { get; set; }
+        public string DeliveryAddress { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public IEnumerable<OrderItem> OrderItems { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        InProcess = 0,
+        OnWay = 1,
+        Delivered = 2
+    }
+}
