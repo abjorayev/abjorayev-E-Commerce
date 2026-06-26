e15a7e4db6b262a8aecd66bb0c46cfa83bea9530
Abdusamad <juraevabdusamad517@gmail.com>
2026-06-26 11:04:11 +0500

    repository and log added

---
+++ E-Commerce/DependencyInjections/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using E_Commerce.Repository;
+
+namespace E_Commerce.DependencyInjections
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddProjectServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IECommerceRepository<>), typeof(ECommerceRepository<>));
+            return services;
+        }
+    }
+}
--- E-Commerce/Program.cs
+++ E-Commerce/Program.cs
@@ -1,11 +1,32 @@
+using E_Commerce.DependencyInjections;
 using ECommerce.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+var solutionDir = Directory.GetParent(builder.Environment.ContentRootPath)!.FullName;
+var logPath = Path.Combine(solutionDir, "logs", "log-.log");
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(
+        path: logPath,
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 30,
+        shared: true
+    )
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+Log.Information("=== ECommerce API Starting ===");
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddProjectServices();
 builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("ECommerceConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
